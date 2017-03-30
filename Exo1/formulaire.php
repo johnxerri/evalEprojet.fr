90338df596b77de2dfe5f529dfd0b0d0910e1409
John <john.s.xerri@gmail.com>
2017-03-30 18:41:06 +0200

    first commit - exo 1

---
+++ Exo1/formulaire.php
@@ -0,0 +1,28 @@
+<?php
+
+// Exercice 1.1 :
+// Pour cet exercice, nous allons créer un formulaire en HTML avec une récupération des saisies en PHP.
+// L'objectif est de récupérer les saisies postées sur la même page (juste au dessus du formulaire).
+//  Créez une page « formulaire.php ».
+
+// nom
+// prenom
+// adresse
+// ville
+// code postal
+// sexe : homme / femme
+// description
+
+// envoie
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Exercice 1</title>
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	</head>
+	<body>
+
+	</body>
+</html>
